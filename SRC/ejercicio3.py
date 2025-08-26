d3dd8f062ad47abbec913640715373d0a740f7a7
Santiago Diaz <santiago.diazs@upb.edu.co>
2025-08-26 11:27:05 -0500

    Se agrega ejercicio con condicional doble

--- SRC/ejercicio3.py
+++ SRC/ejercicio3.py
@@ -6,5 +6,20 @@ print(numero % 3)
 if numero % 3 == 0:
     print(f"El numero {numero} es divisible por 3")
 
+#EJERCICIO CON MENU
+print("=== MENÚ PRINCIPAL ===")
+print("'A'. Ver datos de sensores")
+print("'B'. Configurar parámetros")
+print("'C'. Salir")
 
+opcion = (input("Selecciona una opción: "))
 
+match opcion:
+    case 'A':
+        print("Mostrando datos de sensores...")
+    case 'B':
+        print("Entrando a configuración...")
+    case 'c':
+        print("Saliendo del programa...")
+    case _:
+        print("Opción inválida.")
