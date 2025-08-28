4bc227ddfcd1632f869b2bb4b7822cd379261082
Santiago Diaz <santiago.diazs@upb.edu.co>
2025-08-28 11:32:33 -0500

    se agrega potencia a la calculadora

--- SRC/calculadora.py
+++ SRC/calculadora.py
@@ -1,14 +1,12 @@
 #Calculadora
+#control = True #la variable de control fue eliminada, en el caso E se elimino
 
-
-
-control = True
-
-while control == True:
+while True: #bucle infinito, siempre sera verdadero
     num1 = int(input("Ingrese el primer numero: "))
     num2 = int(input("Ingrese el segundo numero: ")) 
     print("S, Sumar\nR. Restar\nM. Multiplicar\nD. Dividir\nE. Salir")
     opcion = input("Elija una opcion: ")
+    opcion = opcion.upper()  #Para que lo que ingrese el usuario este en mayuscula
     match opcion:
         case 'S':
             print("Suma")
@@ -23,7 +21,10 @@ while control == True:
             print("Division")
             Resultado = num1 / num2
         case 'E': 
-            control = False
+            break
+        case 'P':
+            print("Potenciacion")
+            Resultado = num1 ** num2 
         case _:
             print("Modo No Valido")
     print(f"Resultado = {Resultado}")
