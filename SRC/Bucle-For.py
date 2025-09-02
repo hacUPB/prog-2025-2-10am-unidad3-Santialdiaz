89bfa255e79d0d528328400719bddaf38be8b7d1
Santiago Diaz <santiago.diazs@upb.edu.co>
2025-09-02 11:39:45 -0500

    se agregan actualizaciones

--- SRC/Bucle-For.py
+++ SRC/Bucle-For.py
@@ -1,4 +1,4 @@
-
+'''
 numero = int(input('Ingrese el numero entero: '))
 while numero < 0:
     numero = int(input('Ingrese el numero entero positivo: '))
@@ -7,5 +7,11 @@ for cont in range(1,numero+1):
     if cont % 2 == 0:
         acum += cont
 print(f"La suma de los pares es {acum}")
+'''
 
+mensaje = "Universidad Pontifica Bolivariana"
+numero = int(input('Ingrese el numero entero positivo: '))
+for i in range (numero):
+    print(mensaje)
 
+#EN ESTE CASO LA i SE USA SOLO POR LA SINTAXIS
