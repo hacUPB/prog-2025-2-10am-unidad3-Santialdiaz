04f851976b25c0e0448056c522a053bc6ce3bde7
Santiago Diaz <santiago.diazs@upb.edu.co>
2025-09-04 10:43:57 -0500

    Se agrega ejercicio de temperatura conversion

--- SRC/Bucle-For.py
+++ SRC/Bucle-For.py
@@ -8,10 +8,34 @@ for cont in range(1,numero+1):
         acum += cont
 print(f"La suma de los pares es {acum}")
 '''
-
+'''
 mensaje = "Universidad Pontifica Bolivariana"
 numero = int(input('Ingrese el numero entero positivo: '))
 for i in range (numero):
     print(mensaje)
 
 #EN ESTE CASO LA i SE USA SOLO POR LA SINTAXIS
+'''
+# Conversor de Temperatura
+
+opcion = 'L'                     #Valor temporal
+while opcion != 'Q':
+    opcion = input("F, Fahrenheit a Celsius\nC. Celsius a Fahrenheit\nQ. Salir\n")
+    opcion = opcion.upper()
+    if opcion != 'Q':
+        temperatura = float(input("Ingrese la temperatura a convertir: "))
+        match opcion:
+            case 'f':
+                conversion = (temperatura - 32)*(5/9)
+                print(f"{temperatura}°F = {conversion}°C")
+            case 'C':
+                conversion = (temperatura * 9 / 5) + 32
+                print (f"{temperatura}°C = {conversion}°F")
+            case 'Q':
+                print("Saliendo del programa . . .")
+            case _:
+                print("Opcion no valida")
+    else:
+        print("Saliendo del programa... ")
+      
+
